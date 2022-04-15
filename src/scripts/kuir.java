cb9482d758ea99ab7f4c3f00d7a5b66dc020ce2e
kang yeon joo <rkdduswn0409@gmail.com>
2022-04-15 15:03:12 +0900

    -s 부분 다시 생성

--- src/scripts/kuir.java
+++ src/scripts/kuir.java
@@ -38,7 +38,10 @@ public class kuir {
 			indexer index = new indexer(path);
 			index.invertedXml();
 		}
-
+		else if(command.equals("-s")) {
+			searcher search = new searcher(path);
+			search.InnerProduct(query);
+		}
 
 }
 }
